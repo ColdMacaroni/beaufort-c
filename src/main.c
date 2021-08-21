2e27efae5ccfac0f382eb48cf8a2b7c01b465001
muffin <squishysparrow7.7@gmail.com>
2021-08-21 22:33:53 +1200

    Arg and no args for key and text now working

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ main(int argc, char **argv)
     memset(key, 0, sizeof(char) * BUFSIZ);
     memset(txt, 0, sizeof(char) * BUFSIZ);
 
-    FILE *txt_file = NULL;
+    FILE *txt_file = stdin;
 
     // Check args
     if (argc > 1)
@@ -42,14 +42,25 @@ main(int argc, char **argv)
         // If key flag is set
         else if (key[0] != 0 && argc > 3)
             txt_file = fopen(argv[3], "r");
-        else
-            txt_file = stdin;
     }
 
-    
+    // Read from stdin if not given via argument
+    if (key[0] == 0)
+    {
+        char *tmp_key = calloc(sizeof(char), BUFSIZ);
 
+        printf("Enter key: ");
+        key_copy(fgets(tmp_key, BUFSIZ, stdin), &key);
+
+        free(tmp_key);
+    }
+
+    // Read from stdin if not given via argument
     if (txt_file != NULL)
+    {
+        printf("Enter text. EOF to end input: ");
         read_until_eof(txt_file, &txt, BUFSIZ - 1);
+    }
 
     printf("Key: %s\n", key);
     printf("Text: %s\n", txt);
